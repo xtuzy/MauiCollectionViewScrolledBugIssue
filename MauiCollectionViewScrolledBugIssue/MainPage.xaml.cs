5845ae33ccb38176c1ffd693297f219a2f7ec4cb
yang zhou <xtuzhouyang@outlook.com>
2023-01-13 17:48:43 +0800

    添加项目文件。

---
+++ MauiCollectionViewScrolledBugIssue/MainPage.xaml.cs
@@ -0,0 +1,214 @@
+﻿using ApplePDF.PdfKit;
+using CommunityToolkit.Mvvm.ComponentModel;
+using PDFiumCore;
+using SkiaSharp;
+using SkiaSharp.Views.Maui.Controls;
+
+namespace MauiCollectionViewScrolledBugIssue;
+
+public partial class MainPage : ContentPage
+{
+    /// <summary>
+    /// 缩略图宽度
+    /// </summary>
+    public const int DesignThumbnailWidth = 300;
+
+    PDFThumbnailViewViewModel viewModel;
+    public MainPage()
+    {
+        InitializeComponent();
+        collectionView.Scrolled += CollectionView_Scrolled;
+        LoadMauiAsset();
+    }
+
+    async Task LoadMauiAsset()
+    {
+        var stream = await FileSystem.OpenAppPackageFileAsync("Vulkan.pdf");
+        var doc = Pdfium.Instance.LoadPdfDocument(stream, string.Empty);
+        PDFThumbnailViewViewModel.ItemSize = new Size(DesignThumbnailWidth, DesignThumbnailWidth / doc.GetPageSize(0).Width * doc.GetPageSize(0).Height);
+        viewModel = new PDFThumbnailViewViewModel(doc);
+        collectionView.BindingContext = viewModel;
+        collectionView.ItemsSource = viewModel.PageImages;
+    }
+
+    int lastFirstVisibleItemIndex = -1;
+    int lastLastVisibleItemIndex = -1;
+    CancellationTokenSource tokenSource2;
+    /// <summary>
+    /// load more new image when scroll.
+    /// remove old image that is disappeared.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void CollectionView_Scrolled(object sender, ItemsViewScrolledEventArgs e)
+    {
+        if (lastFirstVisibleItemIndex == -1)
+        {
+            lastFirstVisibleItemIndex = e.FirstVisibleItemIndex;
+            lastLastVisibleItemIndex = e.LastVisibleItemIndex;
+            return;
+        }
+        if (tokenSource2 != null)
+        {
+            tokenSource2.Cancel();//取消上一个task
+            tokenSource2.Dispose();
+            tokenSource2 = null;
+        }
+        tokenSource2 = new CancellationTokenSource();
+        CancellationToken ct = tokenSource2.Token;
+        var task = Task.Run(() =>
+        {
+            var top = e.FirstVisibleItemIndex;
+            var bottom = e.LastVisibleItemIndex;
+            var latTop = lastFirstVisibleItemIndex;
+            var latBottom = lastLastVisibleItemIndex;
+
+            //如果中间的没有加载, 就加载
+            for (int i = top; i <= bottom; i++)
+            {
+                if (tokenSource2.IsCancellationRequested)
+                    return;
+                if (viewModel.PageImages[i].Image == null)
+                    viewModel.PageImages[i].LoadImage(viewModel.Document, PDFThumbnailViewViewModel.ItemSize);
+            }
+
+            bool topScroll = e.VerticalDelta > 0;
+
+            int notRecycleOrAddMoreImageCountInDisappear = 0;
+            if (topScroll)
+            {
+                //往上滑, 下面的数据是新加载的, 上面的是旧的
+
+                //释放不见的,少释放十个
+                for (var i = top - notRecycleOrAddMoreImageCountInDisappear; i > 0; i--)
+                {
+                    if (tokenSource2.IsCancellationRequested)
+                        return;
+                    if (viewModel.PageImages[i].Image == null)//遇到已经释放了的，那么之前的也已经释放
+                        break;
+                    viewModel.PageImages[i].RecycleImage();
+                }
+
+                //多加载10个
+                for (var i = bottom; i < bottom + notRecycleOrAddMoreImageCountInDisappear; i++)
+                {
+                    if (tokenSource2.IsCancellationRequested)
+                        return;
+                    viewModel.PageImages[i].LoadImage(viewModel.Document, PDFThumbnailViewViewModel.ItemSize);
+                }
+            }
+            else
+            {
+                //释放不见的,少释放十个
+                for (var i = bottom + notRecycleOrAddMoreImageCountInDisappear; i < viewModel.PageImages.Count - 1; i++)
+                {
+                    if (tokenSource2.IsCancellationRequested)
+                        return;
+                    if (viewModel.PageImages[i].Image == null)
+                        break;
+                    viewModel.PageImages[i].RecycleImage();
+                }
+
+                //new appear, 加载新增可见的,多加载
+                for (var i = top; i > top - notRecycleOrAddMoreImageCountInDisappear; i--)
+                {
+                    if (tokenSource2.IsCancellationRequested)
+                        return;
+                    viewModel.PageImages[i].LoadImage(viewModel.Document, PDFThumbnailViewViewModel.ItemSize);
+                }
+            }
+
+        }, tokenSource2.Token);
+
+        lastFirstVisibleItemIndex = e.FirstVisibleItemIndex;
+        lastLastVisibleItemIndex = e.LastVisibleItemIndex;
+    }
+
+    public class PDFThumbnailViewViewModel
+    {
+        public static Size ItemSize;//大小需要根据界面大小来
+
+        public PDFThumbnailViewViewModel(PdfDocument doc)
+        {
+            Document = doc;
+            var pageCount = doc.PageCount;
+            PageImages = new List<PageImageContainer>();
+            for (int i = 0; i < pageCount; i++)
+            {
+                var image = new PageImageContainer() { PageIndex = i };
+                PageImages.Add(image);
+            }
+
+            int initImageCount = (int)(DeviceDisplay.MainDisplayInfo.Height / (DesignThumbnailWidth * 1.2)) * 2;//pdf高度一般为高1.2倍
+            Task.Run(() =>
+            {
+                for (int i = 0; i < pageCount; i++)
+                {
+                    if (i < initImageCount)
+                    {
+                        PageImages[i].LoadImage(doc, ItemSize);
+                    }
+                }
+            });
+        }
+
+        public PdfDocument Document { get; set; }
+        /// <summary>
+        /// all page
+        /// </summary>
+        public List<PageImageContainer> PageImages { get; set; }
+    }
+
+    public partial class PageImageContainer : ObservableObject
+    {
+        [ObservableProperty]
+        public int pageIndex;
+
+        [ObservableProperty]
+        public int height;
+
+        [ObservableProperty]
+        public int width;
+        private MemoryStream stream;
+        private SKBitmap bitmap;
+
+        [ObservableProperty]
+        public ImageSource image = null;
+
+        public PageImageContainer()
+        {
+            Height = (int)PDFThumbnailViewViewModel.ItemSize.Height;
+            Width = (int)PDFThumbnailViewViewModel.ItemSize.Width;
+        }
+
+        public void RecycleImage()
+        {
+            stream?.Close();
+            stream = null;
+            if (Image != null)
+                (Image as SKBitmapImageSource).Bitmap = null;
+            Image = null;
+            bitmap?.Dispose();
+            bitmap = null;
+        }
+
+        public void LoadImage(PdfDocument document, Size size)
+        {
+            if (bitmap == null)
+            {
+                var page = document.GetPage(PageIndex);
+                var pageSize = page.GetSize();
+                var flags = (int)(RenderFlags.None);
+                var scale = size.Width / pageSize.Width;//pdfium绘制页面到图片时需要精度，展示时越大，需要越高的精度，因此这里使用当前需要展示的大小与pdf页面基本大小的比
+                bitmap = PdfPageExtension.RenderPageToSKBitmapFormSKBitmap(page, (float)scale, flags, SKColors.White);
+                if (Image == null)
+                {
+                    var source = new SKBitmapImageSource();
+                    source.Bitmap = bitmap;
+                    Image = source;
+                }
+            }
+        }
+    }
+}
+
---
+++ MauiCollectionViewScrolledBugIssue/PdfPageExtension.cs
@@ -0,0 +1,219 @@
+﻿using ApplePDF.PdfKit;
+using PDFiumCore;
+using SkiaSharp;
+using System;
+using System.Runtime.InteropServices;
+
+namespace MauiCollectionViewScrolledBugIssue
+{
+    public static class PdfPageExtension
+    {
+        private static readonly object @lock = new object();
+        /// <summary>
+        /// Pdfium开辟内存先创建Pdfium's Bitmap, 然后拷贝数据到数组, 之后可能需要拷贝数组数据到SKBitmap(不理解InstallPixels是否需要)? 因此,其至少有一次拷贝.
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="density"></param>
+        /// <param name="renderFlags"></param>
+        /// <returns></returns>
+        public static SKBitmap RenderPageToSKBitmapFormPdfiumImage(PdfPage page, float density = 2, int renderFlags = (int)RenderFlags.RenderAnnotations)
+        {
+            var bmp = new SKBitmap();
+
+            //Skiasharp方法
+            // pin the managed array so that the GC doesn't move it
+            var bounds = page.GetSize();
+            var rawBytes = page.Draw(density, density, renderFlags);
+            var gcHandle = GCHandle.Alloc(rawBytes, GCHandleType.Pinned);
+            // install the pixels with the color type of the pixel data
+            int width = (int)(bounds.Width * density);
+            int height = (int)(bounds.Height * density);
+            var info = new SKImageInfo(width, height, SKColorType.Bgra8888, SKAlphaType.Premul);
+            bmp.InstallPixels(info, gcHandle.AddrOfPinnedObject(), info.RowBytes, delegate { gcHandle.Free(); }, null);
+
+            return bmp;
+        }
+
+        public static Stream RenderPageToStreamFormPdfiumImage(PdfPage page, float density = 2, int renderFlags = (int)RenderFlags.RenderAnnotations)
+        {
+            // pin the managed array so that the GC doesn't move it
+            var bounds = page.GetSize();
+            var rawBytes = page.Draw(density, density, renderFlags);
+            var gcHandle = GCHandle.Alloc(rawBytes, GCHandleType.Pinned);
+            var memoryStream = new MemoryStream(rawBytes);
+            return memoryStream;
+        }
+
+        /// <summary>
+        /// SKBitmap开辟内存, 然后Pdfium直接写入该内存
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="density"></param>
+        /// <param name="renderFlags"></param>
+        /// <param name="backgroundColor"></param>
+        /// <returns></returns>
+        public static SKBitmap RenderPageToSKBitmapFormSKBitmap(PdfPage page, float density = 2, int renderFlags = (int)RenderFlags.RenderAnnotations, SKColor backgroundColor = default)
+        {
+            SKBitmap bmp;
+
+            //Skiasharp方法
+            // pin the managed array so that the GC doesn't move it
+            var bounds = page.GetSize();
+            int width = (int)(bounds.Width * density);
+            int height = (int)(bounds.Height * density);
+            var info = new SKImageInfo(width, height, SKColorType.Bgra8888, SKAlphaType.Premul);
+            bmp = new SKBitmap(info);
+            if (backgroundColor != default)
+            {
+                using (var canvas = new SKCanvas(bmp))
+                {
+                    canvas.Clear(backgroundColor);
+                }
+            }
+            page.Draw(bmp.GetPixels(), density, density, 0, renderFlags);
+
+            return bmp;
+        }
+
+#if ANDROID || IOS || MACCATALYST
+        static unsafe void BGRA32ToARGB32(IntPtr pointer, int width, int height)
+        {
+            byte* bgra = (byte*)pointer;
+            int io = 0;
+            byte temp;
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    temp = bgra[io + 0];
+                    bgra[io + 0] = bgra[io + 3]; // a b
+                    bgra[io + 3] = temp; // b a
+                    temp = bgra[io + 1];
+                    bgra[io + 1] = bgra[io + 2]; // r g
+                    bgra[io + 2] = temp; // g r
+
+                    io += 4;
+                }
+            }
+        }
+
+        static unsafe void BGRA32ToRGBA32(IntPtr pointer, int width, int height)
+        {
+            byte* bgra = (byte*)pointer;
+            int io = 0;
+            byte temp;
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    temp = bgra[io + 0];
+                    bgra[io + 0] = bgra[io + 2];
+                    bgra[io + 2] = temp;
+
+                    io += 4;
+                }
+            }
+        }
+
+#if ANDROID
+        /// <summary>
+        /// AndroidBitmap开辟内存, 然后Pdfium直接写入该内存
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="density"></param>
+        /// <param name="renderFlags"></param>
+        /// <param name="backgroundColor"></param>
+        /// <returns></returns>
+        public static Android.Graphics.Bitmap RenderPageToAndroidBitmapFormAndroidBitmap(PdfPage page, float density = 2, int renderFlags = (int)RenderFlags.RenderAnnotations, SKColor backgroundColor = default)
+        {
+            Android.Graphics.Bitmap bmp;
+
+            //Skiasharp方法
+            // pin the managed array so that the GC doesn't move it
+            var bounds = page.GetSize();
+            int width = (int)(bounds.Width * density);
+            int height = (int)(bounds.Height * density);
+            //bmp = new SKBitmap(new SKImageInfo(width, height, SKColorType.Bgra8888, SKAlphaType.Premul));
+            bmp = Android.Graphics.Bitmap.CreateBitmap(width, height, Android.Graphics.Bitmap.Config.Argb8888);
+
+            if (backgroundColor != default)
+            {
+                using (var canvas = new Android.Graphics.Canvas(bmp))
+                {
+                    canvas.DrawColor(SkiaSharp.Views.Android.AndroidExtensions.ToColor(backgroundColor));
+                }
+            }
+            page.Draw(bmp.LockPixels(), density, density, 0, renderFlags);//pdfium的数据是bgra
+
+            BGRA32ToARGB32(bmp.LockPixels(), width, height);//pdfium的数据是bgra, 这里转换成argb
+
+            return bmp;
+        }
+#elif IOS || MACCATALYST
+        /// <summary>
+        /// AndroidBitmap开辟内存, 然后Pdfium直接写入该内存
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="density"></param>
+        /// <param name="renderFlags"></param>
+        /// <param name="backgroundColor"></param>
+        /// <returns></returns>
+        public static IOSCGImage RenderPageToBitmapFormAndroidBitmap(PdfPage page, float density = 2, int renderFlags = (int)RenderFlags.RenderAnnotations, SKColor backgroundColor = default)
+        {
+            // pin the managed array so that the GC doesn't move it
+            var bounds = page.GetSize();
+            int width = (int)(bounds.Width * density);
+            int height = (int)(bounds.Height * density);
+            //bmp = new SKBitmap(new SKImageInfo(width, height, SKColorType.Bgra8888, SKAlphaType.Premul));
+            var _colorSpace = CoreGraphics.CGColorSpace.CreateDeviceRGB();
+            int bytesPerRow = width * 4;
+            int bitmapByteCount = bytesPerRow * height;
+            const int bitsPerComponent = 8;
+            CoreGraphics.CGBitmapFlags flags = CoreGraphics.CGBitmapFlags.PremultipliedLast | CoreGraphics.CGBitmapFlags.ByteOrder32Big;
+
+            var _bitmapData = new byte[bitmapByteCount];
+            var _handle = GCHandle.Alloc(_bitmapData, GCHandleType.Pinned);
+
+            var _context = new CoreGraphics.CGBitmapContext(_bitmapData, width, height, bitsPerComponent, bytesPerRow, _colorSpace, flags);
+            if (backgroundColor != default)
+            {
+                _context.SetFillColor(SkiaSharp.Views.iOS.AppleExtensions.ToCGColor(backgroundColor));
+                _context.FillRect(new CoreGraphics.CGRect(0, 0, width, height));
+            }
+            page.Draw(_handle.AddrOfPinnedObject(), density, density, 0, renderFlags);//pdfium的数据是bgra
+            var bmp = new IOSCGImage(_handle, _context.ToImage());
+            _context.Dispose();
+            BGRA32ToRGBA32(_handle.AddrOfPinnedObject(), width, height);//pdfium的数据是bgra, 这里转换成rgba
+
+            return bmp;
+        }
+
+        public class IOSCGImage : IDisposable
+        {
+            GCHandle GCHandle;
+
+            public IOSCGImage(GCHandle handle, CoreGraphics.CGImage cGImage)
+            {
+                GCHandle = handle;
+                Image = cGImage;
+            }
+
+            public CoreGraphics.CGImage Image { private set; get; }
+            public void Dispose()
+            {
+                Image?.Dispose();
+                GCHandle.Free();
+            }
+        }
+#endif
+#endif
+
+        public static MemoryStream SKBitmapToStream(this SKBitmap bmp)
+        {
+            var stream = new MemoryStream();
+            bmp.Encode(stream, SKEncodedImageFormat.Png, 100);
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
